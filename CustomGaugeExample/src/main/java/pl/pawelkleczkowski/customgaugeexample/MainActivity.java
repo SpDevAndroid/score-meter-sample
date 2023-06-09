7823a8f3c88c57b12586da12eb1cf8378626f6c7
SpDevAndroid <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>
2023-06-09 14:57:57 +0530

    Score meter sample

--- CustomGaugeExample/src/main/java/pl/pawelkleczkowski/customgaugeexample/MainActivity.java
+++ CustomGaugeExample/src/main/java/pl/pawelkleczkowski/customgaugeexample/MainActivity.java
@@ -15,12 +15,9 @@ import pl.pawelkleczkowski.customgauge.CustomGauge;
 
 public class MainActivity extends AppCompatActivity {
 
-	private CustomGauge gauge1;
-	private CustomGauge gauge2;
 	private CustomGauge gauge3;
 
 	int i;
-	private TextView text1;
 	private TextView text2;
 	
 	@Override
@@ -33,38 +30,26 @@ public class MainActivity extends AppCompatActivity {
 		setSupportActionBar(toolbar);
 
 		Button button = findViewById(R.id.button);
-		gauge1 = findViewById(R.id.gauge1);
-		gauge2 = findViewById(R.id.gauge2);
 		gauge3 = findViewById(R.id.gauge3);
+		text2 = findViewById(R.id.textView2);
 
-		text1  = findViewById(R.id.textView1);
-		text2  = findViewById(R.id.textView2);
-		text1.setText(String.valueOf(gauge1.getValue()));
-		text2.setText(String.valueOf(gauge2.getValue()));
-    	text2.setText(String.valueOf(gauge2.getValue()));
+		text2.setText(String.format(Locale.getDefault(), "%1d/%2d", gauge3.getValue(), gauge3.getEndValue()));
     	
 		button.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				gauge2.setEndValue(800);
-				gauge2.setValue(200);
-				text2.setText(String.format(Locale.getDefault(), "%1d/%2d", gauge2.getValue(), gauge2.getEndValue()));
+
+				text2.setText(String.format(Locale.getDefault(), "%1d/%2d", gauge3.getValue(), gauge3.getEndValue()));
 				new Thread() {
 			        public void run() {
-			        	for (i=0;i<100;i++) {
-					        if (i == 50) {
-						        gauge2.setEndValue(1200);
-					        }
+			        	for (i=0;i<=100;i++) {
 			                try {
 			                    runOnUiThread(new Runnable() {
 			                        @Override
 			                        public void run() {
-			                        	gauge1.setValue(i*10);
-			                        	gauge2.setValue(200 + i*5);
 			                        	gauge3.setValue(i);
-			                        	text1.setText(String.valueOf(gauge1.getValue()));
-			                        	text2.setText(String.format(Locale.getDefault(), "%1d/%2d", gauge2.getValue(), gauge2.getEndValue()));
+			                        	text2.setText(String.format(Locale.getDefault(), "%1d/%2d", gauge3.getValue(), gauge3.getEndValue()));
 			                        }
 			                    });
 			                    Thread.sleep(50);
